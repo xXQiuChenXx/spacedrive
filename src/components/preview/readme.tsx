c36f0e8c72ed860620c7b321cb180d21e4eaba97
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-31 23:25:29 +0800

    Update readme.tsx

--- src/components/preview/readme.tsx
+++ src/components/preview/readme.tsx
@@ -3,12 +3,16 @@ import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import rehypeRaw from "rehype-raw";
+import { BookOpenText } from "lucide-react";
 
 const ReadMePreview = ({ content }: { content: string }) => {
   return (
     <div className="w-11/12 mx-auto border p-10 mt-16 prose dark:prose-invert max-w-none rounded-lg shadow">
-      <p className="font-bold text-base text-black dark:text-white">README.md</p>
-      <hr className="mt-0 mb-5"/>
+      <p className="font-bold text-base text-black dark:text-white">
+        <BookOpenText className="size-6 inline-block mr-4" />
+        README.md
+      </p>
+      <hr className="mt-0 mb-5" />
       <Markdown
         remarkPlugins={[remarkGfm, remarkMath]}
         rehypePlugins={[rehypeKatex, rehypeRaw]}
