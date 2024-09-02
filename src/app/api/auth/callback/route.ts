919e57f1e9fc1b78acbbae19e9e1820643c5f42f
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-09-02 13:33:02 +0800

    callback route

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,22 +1,25 @@
 import { exchangeCode } from "@/lib/oAuthHandler";
 import { redirect } from "next/navigation";
 import { type NextRequest } from "next/server";
-import { db } from "@/db";
-import { credentials } from "@/db/schema";
-import { saveToken } from "@/lib/oAuthStore";
+import { saveTokenToDB } from "@/lib/oAuthStore";
 
 export async function GET(request: NextRequest) {
   const searchParmas = request.nextUrl.searchParams;
   const code = searchParmas.get("code");
 
-  if (!code) {
-    console.log("code not found");
-    return redirect("/setup/step-3?error=Code Not Found");
-  }
-  try {
-    const { access_token, refresh_token } = await exchangeCode(code);
+  if (!code) return redirect("/setup/step-3?error=Code Not Found");
 
-    await saveToken({ accessToken: access_token, refreshToken: refresh_token });
+  try {
+    const { access_token, refresh_token, expires_in } = await exchangeCode(
+      code
+    );
+
+    await saveTokenToDB({
+      accessToken: access_token,
+      refreshToken: refresh_token,
+      expiredIn: expires_in,
+      issuedAt: Math.floor(Date.now() / 1000),
+    });
   } catch (error) {
     console.log(error);
     return redirect("/setup/step-3?error=Unknown Error");
