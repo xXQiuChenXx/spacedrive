2fe7426fcfcbd63e3d9b94bf60133a0848a549a8
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-09-04 22:00:02 +0800

    Update Code.tsx

--- src/components/preview/Code.tsx
+++ src/components/preview/Code.tsx
@@ -2,13 +2,9 @@
 
 import { ItemsResponse, OriResponse } from "@/lib/driveRequest";
 import { getLanguageByFileName } from "@/lib/getPreviewType";
-import { useTheme } from "next-themes";
 import { useMemo } from "react";
 import { LightAsync as SyntaxHighlighter } from "react-syntax-highlighter";
-import {
-  tomorrowNightEighties,
-  tomorrow,
-} from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import { tomorrowNightEighties } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import PreviewContainer from "./PreviewContainer";
 
 export const CodePreview = ({
@@ -19,13 +15,12 @@ export const CodePreview = ({
   file: OriResponse | ItemsResponse;
 }) => {
   const codeString = useMemo(() => content, [content]);
-  const { theme } = useTheme();
 
   return (
     <PreviewContainer file={file}>
       <SyntaxHighlighter
         language={getLanguageByFileName(file.name)}
-        style={theme === "dark" ? tomorrowNightEighties : tomorrow}
+        style={tomorrowNightEighties}
         customStyle={{ backgroundColor: "#282c34" }}
       >
         {codeString}
