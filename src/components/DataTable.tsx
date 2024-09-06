361250807c2924af104ce5603b103e89f336c090
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-09-06 20:40:34 +0800

    mobile view download btn

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -11,7 +11,7 @@ import {
 import { usePathname } from "next/navigation";
 import { useRouter } from "nextjs-toploader/app";
 import { ItemsResponse } from "@/lib/driveRequest";
-import { ReactNode, useMemo, useState } from "react";
+import { ReactNode, useMemo, useState, useTransition } from "react";
 import { getColumns } from "./table-column/table-column";
 import { flexRender } from "@tanstack/react-table";
 import { useDataTable } from "@/app/hooks/useDataTable";
@@ -25,6 +25,10 @@ import { DataTableToolbar } from "./DataToolbar";
 import { useMediaQuery } from "@/app/hooks/use-media-query";
 import { Button } from "./ui/button";
 import DeleteDialog from "./action-dialog/DeleteDialog";
+import { downloadMultiFiles } from "@/lib/MultiFileDownloader";
+import path from "path";
+import { handleClick } from "@/lib/downloadHandler";
+import { DownloadIcon, LoaderIcon } from "lucide-react";
 
 const DataTable = ({
   data,
@@ -37,14 +41,35 @@ const DataTable = ({
   const pathname = usePathname();
   const isDesktop = useMediaQuery("(min-width: 860px)");
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [isDownloading, startDownloadTransition] = useTransition();
+  const folderName = path
+    .basename(pathname)
+    .replace("home/", "")
+    .replace("home", "");
 
   // Memoize the columns
-  const columns = useMemo(() => getColumns(isDesktop, pathname), [isDesktop, pathname]);
+  const columns = useMemo(
+    () => getColumns(isDesktop, pathname),
+    [isDesktop, pathname]
+  );
   const { table } = useDataTable({ columns, data });
-
+  function onDonwloadClick() {
+    startDownloadTransition(async () => {
+      const items = table
+        .getSelectedRowModel()
+        .rows.map((r) => r.original)
+        .filter((item) => !item.file.isFolder);
+      await downloadMultiFiles({ items, folderName }).catch((err) =>
+        console.log(err)
+      );
+    });
+  }
   return (
     <div className="w-full md:w-11/12 mx-auto overflow-auto">
-      <DataTableToolbar table={table} setShowDeleteDialog={setShowDeleteDialog}/>
+      <DataTableToolbar
+        table={table}
+        setShowDeleteDialog={setShowDeleteDialog}
+      />
       <div className="overflow-hidden rounded-md border mt-2.5">
         <Table>
           <TableHeader>
@@ -121,19 +146,32 @@ const DataTable = ({
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </p>
-      {Boolean(table.getFilteredSelectedRowModel().rows.length) && !isDesktop && (
-        <div className="fixed bottom-0 left-0 mb-6 z-50 w-full">
-          <div className="bg-gray-900 flex justify-center w-fit mx-auto gap-5 border p-3 shodow-lg rounded">
-            <Button
-              variant="destructive"
-              onClick={() => setShowDeleteDialog(true)}
-            >
-              Delete
-            </Button>
-            <Button variant="outline">Download</Button>
+      {Boolean(table.getFilteredSelectedRowModel().rows.length) &&
+        !isDesktop && (
+          <div className="fixed bottom-0 left-0 mb-6 z-50 w-full">
+            <div className="bg-gray-900 flex justify-center w-fit mx-auto gap-5 border p-3 shodow-lg rounded">
+              <Button
+                variant="destructive"
+                onClick={() => setShowDeleteDialog(true)}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="outline"
+                aria-label="Donwload"
+                onClick={onDonwloadClick}
+                disabled={isDownloading}
+              >
+                {isDownloading ? (
+                  <LoaderIcon className="animate-spin mr-2 size-4" />
+                ) : (
+                  <DownloadIcon className="size-4 mr-2" />
+                )}
+                {isDownloading ? "Downloading" : "Download"}
+              </Button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       <DeleteDialog
         items={
           table
