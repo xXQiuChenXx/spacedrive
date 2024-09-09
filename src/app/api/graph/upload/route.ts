ad6fe6817eda25d5d36badfc246c5d631640a51d
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-09-10 01:08:59 +0800

    Update route.ts

--- src/app/api/graph/upload/route.ts
+++ src/app/api/graph/upload/route.ts
@@ -8,7 +8,8 @@ export async function POST(request: NextRequest) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
   const path = formData.get("path") as string;
-  if (!file || !path) return { error: "400 Bad Request" };
+  if (!file || !path)
+    return Response.json({ error: "400 Bad Request" }, { status: 400 });
 
   const token = await getCachedToken();
   if (!token) return redirect("/setup");
