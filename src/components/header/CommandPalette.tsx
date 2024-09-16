d526a907d4464d3e85c669817fcc69c100d2b5df
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-09-16 10:43:22 +0800

    Update CommandPalette.tsx

--- src/components/header/CommandPalette.tsx
+++ src/components/header/CommandPalette.tsx
@@ -49,7 +49,7 @@ export function CommandPalette() {
         <DialogContent className="overflow-hidden p-0">
           <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
             <CommandInput
-              placeholder="Type a command or search..."
+              placeholder="Search for items..."
               onValueChange={setQuery}
             />
             <CommandList>
