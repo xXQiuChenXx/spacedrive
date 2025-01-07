3270f1a2b97ffebec245742565c9e1f8d6fe0d7e
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2025-01-07 20:02:04 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        canvas: "",
+      },
+    },
+  },
 };
 
 export default nextConfig;
